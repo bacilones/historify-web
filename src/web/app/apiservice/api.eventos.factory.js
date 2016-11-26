e9a043c367f39ffc9d8ef799d83d4621531c8e24
cantoz <dcantos@opendrive.cl>
2016-11-26 19:57:12 -0400

    Getting markers data from API

---
+++ src/web/app/apiservice/api.eventos.factory.js
@@ -0,0 +1,31 @@
+(function() {
+'use strict';
+
+    angular
+        .module('historify.apiservice')
+        .factory('Evento', evento);
+
+    evento.$inject = ['$http'];
+    
+    function evento($http) {
+       return {
+            getData: getData
+        };
+        
+        // LISTA:
+        function getData(model){
+            var request = {
+                method: 'GET',
+                url: 'http://api.historify.cl/historicalevent',
+                params: {}
+            };
+            
+            return $http(request)
+                .then(function(response){
+                    return response.data;        
+                });
+        }
+        
+        
+    }
+})();
--- src/web/app/app.js
+++ src/web/app/app.js
@@ -3,39 +3,61 @@
     'use strict';
 
     var app = angular.module('historify', []); 
-    app.controller('MapController', function ($scope) {
+    app.controller('MapController', function ($scope, $http) {
         
-        var events = [
-            ['example 1', -33.434678, -70.635780, 18],
-            ['example 2', -33.434778, -70.635880, 18],
-            ['example 3', -33.434878, -70.635980, 18],
-            ['example 4', -33.434978, -70.636080, 18]
-        ];
+        //var events;
 
         var newMarker = true;
         var milestone = '../resources/img/milestone.png';
         var milestoneAjeno = '../resources/img/milestoneAjeno.png';
 
-        // This example adds a marker to indicate the position of USS,
-        $scope.initMap = function(){
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 18,
-                center: { lat: -33.434678, lng: -70.635780 }
-            });
-        
-            // This event listener calls addMarker() when the map is clicked.
-            google.maps.event.addListener(map, 'click', function (event) {
-                if (newMarker) {
-                    addNewMarker(event.latLng, map);
-                    newMarker = !newMarker;
-                }
-            });
 
-            // localizamos todos los eventos en el mapa
-            for (var i = 0; i < events.length; i++) {
-                var event = events[i];
-                addMarker({ lat: event[1], lng: event[2] }, map, event[0]);
-            }
+        // This example adds a marker to indicate the position of USS,
+
+        $scope.getData = function(){
+	        var request = {
+                method: 'GET',
+                url: 'http://api.historify.cl/historicalevent',
+                params: {}
+            };
+            
+            return $http(request)
+                .then(function(response){
+                    return response.data;   
+                });
+        }
+
+        $scope.initMap = function(){
+        	$scope.getData()
+        		.then(function(events){
+        			console.log(events);
+
+		            var map = new google.maps.Map(document.getElementById('map'), {
+		                zoom: 18,
+		                center: { lat: -33.434678, lng: -70.635780 }
+		            });
+		        
+		            // This event listener calls addMarker() when the map is clicked.
+		            google.maps.event.addListener(map, 'click', function (event) {
+		                if (newMarker) {
+		                    addNewMarker(event.latLng, map);
+		                    newMarker = !newMarker;
+		                }
+		            });
+
+		            // localizamos todos los eventos en el mapa
+		            //for (var evento in events) {
+		            //	console.log(evento);
+		                //addMarker({ lat: evento.lat, lng: evento.long }, map, evento.name);
+		            //}
+		            //angular.each()
+		            events.forEach(function( value){
+		            	addMarker({ lat: Number.parseFloat(value.lat), lng: Number.parseFloat(value.long) }, map, value.name);
+		            })
+
+        		});
+
+        	
         }
 
         // Adds a marker to the map.
