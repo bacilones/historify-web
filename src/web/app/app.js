6a42fc4ab070501ab658f916431053a494e0365f
qnelo <qnelo@hotmail.com>
2016-11-26 18:42:59 -0300

    maps mostrandose en la web

--- src/web/app/app.js
+++ src/web/app/app.js
@@ -4,19 +4,17 @@
 
     var app = angular.module('historify', []); 
     app.controller('MapController', function ($scope) {
-
-        $scope.texto = 'y la wea';
         
-        $scope.events = [
+        var events = [
             ['example 1', -33.434678, -70.635780, 18],
             ['example 2', -33.434778, -70.635880, 18],
             ['example 3', -33.434878, -70.635980, 18],
             ['example 4', -33.434978, -70.636080, 18]
         ];
 
-        $scope.newMarker = true;
-        $scope.milestone = './milestone.png';
-        $scope.milestoneAjeno = './milestoneAjeno.png';
+        var newMarker = true;
+        var milestone = '../resources/img/milestone.png';
+        var milestoneAjeno = '../resources/img/milestoneAjeno.png';
 
         // This example adds a marker to indicate the position of USS,
         $scope.initMap = function(){
@@ -37,36 +35,35 @@
             for (var i = 0; i < events.length; i++) {
                 var event = events[i];
                 addMarker({ lat: event[1], lng: event[2] }, map, event[0]);
-                console.log('eventos', event);
             }
+        }
+
+        // Adds a marker to the map.
+        function addMarker(location, map, event) {
+            // Add the marker at the clicked location, and add the next-available label
+            // from the array of alphabetical characters.
+            var marker = new google.maps.Marker({
+                position: location,
+                map: map,
+                icon: milestoneAjeno,
+                label: event
+            });
+        }
+
+        // Adds a marker to the map.
+        function addNewMarker(location, map) {
+            // Add the marker at the clicked location, and add the next-available label
+            // from the array of alphabetical characters.
+            var marker = new google.maps.Marker({
+                position: location,
+                label: 'nuevo',
+                map: map,
+                draggable: true,
+                icon: milestone
+            });
         }
 
         $scope.initMap();
     });
-
-    // Adds a marker to the map.
-    function addMarker(location, map, event) {
-        // Add the marker at the clicked location, and add the next-available label
-        // from the array of alphabetical characters.
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            icon: milestoneAjeno,
-            label: event
-        });
-    }
-
-    // Adds a marker to the map.
-    function addNewMarker(location, map) {
-        // Add the marker at the clicked location, and add the next-available label
-        // from the array of alphabetical characters.
-        var marker = new google.maps.Marker({
-            position: location,
-            label: 'nuevo',
-            map: map,
-            draggable: true,
-            icon: milestone
-        });
-    }
 
 })();
